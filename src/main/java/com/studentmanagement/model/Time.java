dc20a78c50c9833b618329532aa6635ff80f0d6f
MinhQuanThach <thachminhquan110905@gmail.com>
2024-12-01 09:42:02 +0700

    Change Takes, Time id field to auto increment

--- src/main/java/com/studentmanagement/model/Time.java
+++ src/main/java/com/studentmanagement/model/Time.java
@@ -14,6 +14,7 @@ public class Time {
     // Getters and Setters
     @Id
     @Column(name = "id_time", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idTime;
 
     @ManyToOne
